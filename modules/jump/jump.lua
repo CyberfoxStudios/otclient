b6508126e6d8958aeb68a362e133b9fc86e3effa
JTMintsASU <64926558+CyberfoxStudios@users.noreply.github.com>
2024-04-21 13:29:42 -0700

    Remove jump module questline window and references
    Also refactors some top-level questlog terminology to jump

--- modules/jump/jump.lua
+++ modules/jump/jump.lua
@@ -1,33 +1,26 @@
-questLogButton = nil
-questLineWindow = nil
+jumpButton = nil
 
 function init()
   g_ui.importStyle('jumpwindow')
   g_ui.importStyle('jumplinewindow')
 
-  questLogButton = modules.client_topmenu.addLeftGameButton('questLogButton', tr('Quest Log'), '/images/topbuttons/jump', function() g_game.requestQuestLog() end)
+  jumpButton = modules.client_topmenu.addLeftGameButton('jumpButton', tr('Jump'), '/images/topbuttons/jump', function() g_game.requestQuestLog() end)
 
   connect(g_game, { onQuestLog = onGameQuestLog,
-                    onQuestLine = onGameQuestLine,
                     onGameEnd = destroyWindows})
 end
 
 function terminate()
   disconnect(g_game, { onQuestLog = onGameQuestLog,
-                       onQuestLine = onGameQuestLine,
                        onGameEnd = destroyWindows})
 
   destroyWindows()
-  questLogButton:destroy()
+  jumpButton:destroy()
 end
 
 function destroyWindows()
   if questLogWindow then
     questLogWindow:destroy()
-  end
-
-  if questLineWindow then
-    questLineWindow:destroy()
   end
 end
 
@@ -43,10 +36,6 @@ function onGameQuestLog(quests)
     local questLabel = g_ui.createWidget('QuestLabel', questList)
     questLabel:setOn(completed)
     questLabel:setText(name)
-    questLabel.onDoubleClick = function()
-      questLogWindow:hide()
-      g_game.requestQuestLine(id)
-    end
   end
 
   questLogWindow.onDestroy = function()
@@ -55,34 +44,3 @@ function onGameQuestLog(quests)
 
   questList:focusChild(questList:getFirstChild())
 end
-
-function onGameQuestLine(questId, questMissions)
-  if questLogWindow then questLogWindow:hide() end
-  if questLineWindow then questLineWindow:destroy() end
-
-  questLineWindow = g_ui.createWidget('QuestLineWindow', rootWidget)
-  local missionList = questLineWindow:getChildById('missionList')
-  local missionDescription = questLineWindow:getChildById('missionDescription')
-
-  connect(missionList, { onChildFocusChange = function(self, focusedChild)
-    if focusedChild == nil then return end
-    missionDescription:setText(focusedChild.description)
-  end })
-
-  for i,questMission in pairs(questMissions) do
-    local name, description = unpack(questMission)
-
-    local missionLabel = g_ui.createWidget('MissionLabel')
-    missionLabel:setText(name)
-    missionLabel.description = description
-    missionList:addChild(missionLabel)
-  end
-
-  questLineWindow.onDestroy = function()
-    if questLogWindow then questLogWindow:show() end
-    questLineWindow = nil
-  end
-
-  missionList:focusChild(missionList:getFirstChild())
-end
-

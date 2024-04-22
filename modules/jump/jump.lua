73ede2db004a9759cd8e3e3248cce6d5f442a1ae
JTMintsASU <64926558+CyberfoxStudios@users.noreply.github.com>
2024-04-22 03:42:03 -0700

    Add detectOutOfBounds(), refactor, remove close
    - detectOutOfBounds() is called each button update to see if we have reached the window boundary and need to reset
    - Externalizes the resetPosition() code for reuse

--- modules/jump/jump.lua
+++ modules/jump/jump.lua
@@ -25,24 +25,36 @@ function onGameJump()
   jumpWindow = g_ui.createWidget('JumpWindow', rootWidget)
   interactButton = jumpWindow:recursiveGetChildById('interactButton')
 
+  function resetPosition()
+    -- resets button position
+    -- randomize height. lower is wider range
+    local divFactor = 4 * math.random(-1, 1)
+    local newHeight = interactButton:getParent():getHeight() / divFactor
+    interactButton:setMarginTop(newHeight)
+    interactButton:setMarginRight(0)
+  end
+
+  function detectOutOfBounds()
+    -- take action once the button is leaving the window
+    local maxMargin = interactButton:getParent():getWidth() - 100 -- offset
+    if (interactButton:getMarginRight() > maxMargin) then
+      resetPosition()
+    end
+  end
+
   local currentMargin = nil
   local moveButtonEvent = nil
   function moveButtonLeft()
     -- This function continuously moves the interact button left
+    detectOutOfBounds() -- keep in window
     currentMargin = interactButton:getMarginRight()
     interactButton:setMarginRight(currentMargin + 5)
     moveButtonEvent = scheduleEvent(moveButtonLeft, 50) --50 ms
   end
   moveButtonLeft()
 
-  interactButton.onClick = function()
-    -- resets button position onClick
-    -- randomize height. lower is wider range
-    local divFactor = 4 * math.random(-1, 1)
-    local newHeight = interactButton:getParent():getHeight() / divFactor
-    interactButton:setMarginTop(newHeight)
-
-    interactButton:setMarginRight(0)
+  interactButton.onClick = function ()
+    resetPosition()
   end
 
   interactButton.onDestroy = function()

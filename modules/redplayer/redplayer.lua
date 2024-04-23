9037dd870e0313284188abc7ed87a070c85c3a40
JTMintsASU <64926558+CyberfoxStudios@users.noreply.github.com>
2024-04-23 01:09:03 -0700

    Toggle color on keypress
    Adds code to redplayer that will color red when pressed and color white ("blank") when let go.
    Unbinds keys to properly cleanup when the module is terminated.

--- modules/redplayer/redplayer.lua
+++ modules/redplayer/redplayer.lua
@@ -1,14 +1,26 @@
 function init()
-  toggleColor()
+  g_keyboard.bindKeyDown('R', redPlayer)
+  g_keyboard.bindKeyUp('R', whitePlayer)
 end
 
 function terminate()
+  --undo bindings
+  g_keyboard.unbindKeyDown('R')
+  g_keyboard.unbindKeyUp('R')
 end
 
-function toggleColor()
+function redPlayer()
   -- changes player color
   local player = g_game.getLocalPlayer()
   if player then
     player:setOutfitColor('#f55e5e', 0) -- red
+  end
+end
+
+function whitePlayer()
+  -- changes player color
+  local player = g_game.getLocalPlayer()
+  if player then
+    player:setOutfitColor('#ffffff', 0) -- red
   end
 end
